c2664b88c7df0f1b29f1b718387822248e0f8645
Klaus, Adam <49078569+atklaus@users.noreply.github.com>
2026-02-18 15:38:34 -0600

    Update iceberg

--- analytics/dbt/macros/r2_iceberg.sql
+++ analytics/dbt/macros/r2_iceberg.sql
@@ -1,4 +1,6 @@
 {% macro attach_r2_iceberg_if_needed() %}
+  {{ log("attach_r2_iceberg_if_needed invoked, target=" ~ target.name, info=True) }}
+
   {% if target.name != "iceberg" %}
     {{ log("Skipping R2 Iceberg attach (target is not iceberg).", info=True) }}
     {{ return("select 1") }}
@@ -8,7 +10,13 @@
   {% set warehouse = env_var("R2_ICEBERG_WAREHOUSE", "") %}
   {% set token = env_var("R2_ICEBERG_TOKEN", "") %}
 
-  {% if not catalog_uri or not warehouse or not token %}
+  {% set has_catalog_uri = (catalog_uri | length) > 0 %}
+  {% set has_warehouse = (warehouse | length) > 0 %}
+  {% set has_token = (token | length) > 0 %}
+
+  {{ log("R2 Iceberg env present? catalog_uri=" ~ has_catalog_uri ~ " warehouse=" ~ has_warehouse ~ " token=" ~ has_token, info=True) }}
+
+  {% if not has_catalog_uri or not has_warehouse or not has_token %}
     {{ log("R2 Iceberg env vars missing; skipping attach for target iceberg.", info=True) }}
     {{ return("select 1") }}
   {% endif %}
@@ -23,16 +31,20 @@
     {% do run_query("INSTALL iceberg") %}
     {% do run_query("LOAD iceberg") %}
     {% do run_query("DROP SECRET IF EXISTS r2_iceberg_secret") %}
-    {% do run_query("CREATE SECRET r2_iceberg_secret (TYPE iceberg, TOKEN '" ~ safe_token ~ "')") %}
+    {% do run_query("CREATE SECRET r2_iceberg_secret (TYPE ICEBERG, TOKEN '" ~ safe_token ~ "')") %}
     {% do run_query("DETACH DATABASE IF EXISTS r2_iceberg") %}
-    {% do run_query("ATTACH '" ~ safe_warehouse ~ "' AS r2_iceberg (TYPE iceberg, ENDPOINT '" ~ safe_catalog_uri ~ "', SECRET r2_iceberg_secret)") %}
-    {{ log("Attached R2 Iceberg catalog as r2_iceberg.", info=True) }}
+    {% do run_query("ATTACH '" ~ safe_warehouse ~ "' AS r2_iceberg (TYPE ICEBERG, ENDPOINT '" ~ safe_catalog_uri ~ "', SECRET r2_iceberg_secret)") %}
+
+    {% set dbs = run_query("select database_name from duckdb_databases()") %}
+    {% if dbs %}
+      {{ log(dbs.table | string, info=True) }}
+    {% endif %}
   {% endif %}
 
   {{ return("select 1") }}
 {% endmacro %}
 
-{% macro debug_attached_catalogs() %}
+{% macro debug_duckdb_databases() %}
   {% set query = "select * from duckdb_databases()" %}
   {% if execute %}
     {% set results = run_query(query) %}
